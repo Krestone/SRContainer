34cdb84d983032c8a80faf04f787fb6ce13ebaf0
root <yunus.cukran@gmail.com>
2018-12-02 06:32:11 +0000

    cgroups write to root's sys. can run shell without memory flag, cant fint bash w/ memory flag

--- sr_container.c
+++ sr_container.c
@@ -106,9 +106,10 @@ int main(int argc, char **argv)
             struct cgroup_setting* mem_setting = malloc(sizeof(struct cgroup_setting));
             struct cgroup_setting** mem_settings = malloc(sizeof(struct cgroup_setting *));
             
+
             strcpy(mem->control,CGRP_MEMORY_CONTROL);
             strcpy(mem_setting->name,"memory.limit_in_bytes");
-            strcpy(mem_setting->value,"102400000");
+            strcpy(mem_setting->value,"1024000");
             
             *(mem_settings) = mem_setting;
 	    *(mem_settings+1) = &self_to_task;
